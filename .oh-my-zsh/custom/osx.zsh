facd559e324099a5b65ccc6ac5f61a013dd3737a
Igor Motov <igor@motovs.org>
2013-10-09 10:52:27 +0200

    Cleanup

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -3,8 +3,8 @@ OS=$(uname)
 if [[ $OS == "Darwin" ]]; then
 
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
-    CLOJURE_HOME=/usr/local/clojure; export CLOJURE_HOME
     GRADLE_HOME=/usr/local/gradle; export GRADLE_HOME
-    PATH=~/bin:$GROOVY_HOME/bin:$CLOJURE_HOME/script:$GRADLE_HOME/bin:$PATH; export PATH
+    PATH=~/bin:$GROOVY_HOME/bin:$GRADLE_HOME/bin:$PATH; export PATH
+    export JAVA_HOME=$(/usr/libexec/java_home)
     export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 fi
--- .oh-my-zsh/custom/sonian.zsh
+++
@@ -1,3 +0,0 @@
- export OPSCODE_USER=sonian_devs
- export SONIAN_USER=igor
- 
