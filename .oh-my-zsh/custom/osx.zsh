f15bb091eaed6cdc1f911da5ae98fc21a258a68a
Igor Motov <igor@motovs.org>
2013-11-16 16:33:19 -0500

    Cleanup OS X settings for Mavericks

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -1,10 +1,9 @@
 OS=$(uname)
 
 if [[ $OS == "Darwin" ]]; then
-
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
-    GRADLE_HOME=/usr/local/gradle; export GRADLE_HOME
-    PATH=~/bin:$GROOVY_HOME/bin:$GRADLE_HOME/bin:$PATH; export PATH
-    export JAVA_HOME=$(/usr/libexec/java_home)
-    export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+	M2_HOME=/usr/local/apache-maven-3.1.1; export M2_HOME
+	MAVEN_OPTS="-Xms256m -Xmx512m"; export MAVEN_OPTS
+    JAVA_HOME=$(/usr/libexec/java_home); export JAVA_HOME
+    PATH=/opt/local/bin:/opt/local/sbin:~/bin:$GROOVY_HOME/bin:$M2_HOME/bin:$PATH; export PATH
 fi
