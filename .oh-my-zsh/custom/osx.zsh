d749fb0cc35090c155d0e8a760ff736412c766ad
Igor Motov <igor@motovs.org>
2014-03-17 14:01:59 -0400

    Switch paths to work with homebrew

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -3,6 +3,6 @@ OS=$(uname)
 if [[ $OS == "Darwin" ]]; then
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
     JAVA_HOME=$(/usr/libexec/java_home); export JAVA_HOME
-    PATH=/opt/local/bin:/opt/local/sbin:~/bin:$GROOVY_HOME/bin:$PATH; export PATH
+	PATH=/usr/local/bin:~/bin:$GROOVY_HOME/bin:$PATH; export PATH
 	alias callhome="ssh -C -N -2 -v -D 8080 home"
 fi
