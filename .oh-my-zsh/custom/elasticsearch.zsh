51d17d8831cd441f920a43e4365b43e5669bc007
Igor Motov <igor@motovs.org>
2014-03-19 11:52:55 -0400

    Add Elasticsearch specific aliases

---
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -0,0 +1,6 @@
+# Elasticsearch specific settings
+alias gffes='git merge --ff-only $(current_branch)'
+alias gffor='git merge --ff-only $(current_branch)'
+alias gfees='git fetch elasticsearch'
+alias gfeor='git fetch origin'
+alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
