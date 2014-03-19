f388087ba254648a4fee5aec8b90b0a8eccfd56b
Igor Motov <igor@motovs.org>
2014-03-19 11:34:34 -0400

    Add callhome script

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -4,4 +4,5 @@ if [[ $OS == "Darwin" ]]; then
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
     JAVA_HOME=$(/usr/libexec/java_home); export JAVA_HOME
     PATH=/opt/local/bin:/opt/local/sbin:~/bin:$GROOVY_HOME/bin:$PATH; export PATH
+	alias callhome="ssh -C -N -2 -v -D 8080 home"
 fi
