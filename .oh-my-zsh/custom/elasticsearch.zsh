1fb4cc56a04d5fecdfd454c18b95735cfaeebe1f
Igor Motov <igor@motovs.org>
2014-03-25 09:50:20 -0400

    Fix issue in fast forward alias

--- .oh-my-zsh/custom/elasticsearch.zsh
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -1,6 +1,6 @@
 # Elasticsearch specific settings
-alias gffes='git merge --ff-only $(current_branch)'
-alias gffor='git merge --ff-only $(current_branch)'
+alias gffes='git merge --ff-only elasticsearch/$(current_branch)'
+alias gffor='git merge --ff-only origin/$(current_branch)'
 alias gfees='git fetch elasticsearch'
 alias gfeor='git fetch origin'
 alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
