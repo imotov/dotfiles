a49e20e9f5731f7c16d71c05d3f32572d4aae1eb
Igor Motov <igor@motovs.org>
2014-06-01 19:51:49 -0400

    Add aliases for git commands

--- .oh-my-zsh/custom/elasticsearch.zsh
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -3,4 +3,7 @@ alias gffes='git merge --ff-only elasticsearch/$(current_branch)'
 alias gffor='git merge --ff-only origin/$(current_branch)'
 alias gfees='git fetch elasticsearch'
 alias gfeor='git fetch origin'
+function gpr { 
+    git fetch elasticsearch pull/$1/head:pr/$1
+}
 alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
