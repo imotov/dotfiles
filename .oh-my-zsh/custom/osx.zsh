37f68b47702ceabbf6ccd36709066caa682e8622
Igor Motov <igor@motovs.org>
2014-11-24 23:23:14 -0500

    Switch to java 1.7 by default

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -2,7 +2,7 @@ OS=$(uname)
 
 if [[ $OS == "Darwin" ]]; then
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
-    JAVA_HOME=$(/usr/libexec/java_home); export JAVA_HOME
+    JAVA_HOME=$(/usr/libexec/java_home -v 1.7); export JAVA_HOME
 	PATH=/usr/local/bin:~/bin:$GROOVY_HOME/bin:$PATH; export PATH
 	alias callhome="ssh -C -N -2 -v -D 8080 home"
 fi
