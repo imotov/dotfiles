0f4ac4e3f5b364dce0fa0fc6b574b562b18e0cef
Igor Motov <igor@motovs.org>
2015-05-14 19:47:14 -0400

    Update flea build scripts

--- .oh-my-zsh/custom/elasticsearch.zsh
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -8,4 +8,5 @@ function gpr {
 }
 alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
 alias gnow='git commit --amend --date "`date`"'
-alias fleatest='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn clean test"'
+alias fleatest='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test"'
+alias flearemtest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test"'
--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -3,7 +3,7 @@ OS=$(uname)
 if [[ $OS == "Darwin" ]]; then
 	ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
-    JAVA_HOME=$(/usr/libexec/java_home -v 1.7); export JAVA_HOME
+    JAVA_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_HOME
 	PATH=/usr/local/bin:~/bin:$GROOVY_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
-	alias callhome="ssh -C -N -2 -v -D 8080 home"
+	alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
 fi
