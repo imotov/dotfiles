ff7a0968a310812af61ed60368332c1773795692
Igor Motov <igor@motovs.org>
2015-05-26 15:47:39 -1000

    Add local test commands

--- .oh-my-zsh/custom/elasticsearch.zsh
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -8,5 +8,7 @@ function gpr {
 }
 alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
 alias gnow='git commit --amend --date "`date`"'
+alias localtest='JAVA_HOME=$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test'
+alias local8test='JAVA_HOME=$(/usr/libexec/java_home -v 1.8) mvn -Dtests.slow=true clean test'
 alias fleatest='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test"'
 alias flearemtest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test"'
