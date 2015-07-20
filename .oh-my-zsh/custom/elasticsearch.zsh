498f3563067cf94dc2959ab50f1debccbe72b10f
Igor Motov <igor@motovs.org>
2015-07-20 19:40:28 -0400

    Add scripts for running builds on spider.local

--- .oh-my-zsh/custom/elasticsearch.zsh
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -8,9 +8,16 @@ function gpr {
 }
 alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
 alias gnow='git commit --amend --date "`date`"'
+# Local tests
 alias localtest='JAVA_HOME=$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test'
 alias local8test='JAVA_HOME=$(/usr/libexec/java_home -v 1.8) mvn -Dtests.slow=true clean test'
 alias localbwctest='JAVA_HOME=$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.version=1.5.2 clean test'
+
+# flea tests
 alias fleatest='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean verify"'
 alias flearemtest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean verify"'
-alias flearembwctest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.version=1.5.2 clean verify"'
+alias flearembwctest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.path=/Users/igor/Builds/${PWD##*/}/backwards -Dtests.bwc.version=1.5.2 clean verify"'
+
+# spider tests
+alias spidertest='git push -f spider $(current_branch); ssh spider.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean verify"'
+alias spiderbwctest='git push -f spider $(current_branch); ssh spider.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.path=/Users/igor/Builds/${PWD##*/}/backwards -Dtests.bwc.version=1.5.2 clean verify"'
