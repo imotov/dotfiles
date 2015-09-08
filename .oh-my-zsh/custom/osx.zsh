f46bd2329a02b9529357dafc58d6c83d58f33646
Igor Motov <igor@motovs.org>
2015-09-08 19:53:43 -0400

    Bye, bye java 1.7

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -4,6 +4,8 @@ if [[ $OS == "Darwin" ]]; then
 	ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
     GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
     JAVA_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_HOME
-	PATH=/usr/local/bin:~/bin:$GROOVY_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
-	alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
+    PATH=/usr/local/bin:~/bin:$GROOVY_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
+    GOPATH=~/Projects/GoWork; export GOPATH
+    alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
+    alias agat='for i in img*.tif; do convert -crop 50%x100% +repage $i ${i%.*}%d.jpg; done'
 fi
