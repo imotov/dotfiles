61c80188b05ea9a352ad14d51476cdd85c1a55ad
Igor Motov <igor@motovs.org>
2016-04-07 12:57:33 -0600

    Add ability to run tests remotely

--- .oh-my-zsh/custom/elasticsearch.zsh
+++ .oh-my-zsh/custom/elasticsearch.zsh
@@ -3,21 +3,15 @@ alias gffes='git merge --ff-only elasticsearch/$(current_branch)'
 alias gffor='git merge --ff-only origin/$(current_branch)'
 alias gfees='git fetch elasticsearch'
 alias gfeor='git fetch origin'
-function gpr { 
+function gpr {
     git fetch elasticsearch pull/$1/head:pr/$1
 }
 alias estest="ES_TEST_LOCAL=true mvn -Dtests.jvms=4 clean test"
 alias gnow='git commit --amend --date "`date`"'
-# Local tests
-alias localtest='JAVA_HOME=$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true clean test'
-alias local8test='JAVA_HOME=$(/usr/libexec/java_home -v 1.8) mvn -Dtests.slow=true clean test'
-alias localbwctest='JAVA_HOME=$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.version=1.5.2 clean test'
-
 # flea tests
-alias fleatest='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.8) mvn -Dtests.slow=true clean verify"'
-alias flearemtest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.8) mvn -Dtests.slow=true clean verify"'
-alias flearembwctest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.path=/Users/igor/Builds/${PWD##*/}/backwards -Dtests.bwc.version=1.5.2 clean verify"'
-
-# spider tests
-alias spidertest='git push -f spider $(current_branch); ssh spider.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.8) mvn -Dtests.slow=true clean verify"'
-alias spiderbwctest='git push -f spider $(current_branch); ssh spider.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.7) mvn -Dtests.slow=true -Dtests.bwc=true -Dtests.bwc.path=/Users/igor/Builds/${PWD##*/}/backwards -Dtests.bwc.version=1.5.2 clean verify"'
+alias fleatest='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.8) gradle clean check"'
+alias fleapush='git push -f flea $(current_branch); ssh flea.local "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch)"'
+alias flearempush='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch)"'
+alias flearemtest='git push -f flearemote $(current_branch); ssh flea.homedns.org -p 22422 "cd ~/Builds/${PWD##*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch); JAVA_HOME=\$(/usr/libexec/java_home -v 1.8) gradle clean check"'
+alias beatoff='mv ./buildSrc/src/main/resources/beat.wav ./buildSrc/src/main/resources/beat.wav.bak;mv ./buildSrc/build/resources/main/beat.wav ./buildSrc/build/resources/main/beat.wav.bak'
+alias beaton='mv ./buildSrc/src/main/resources/beat.wav.bak ./buildSrc/src/main/resources/beat.wav;mv ./buildSrc/build/resources/main/beat.wav.bak ./buildSrc/build/resources/main/beat.wav'
