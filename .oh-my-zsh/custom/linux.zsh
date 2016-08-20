475c997cd61c6b4bd0cb2ad79613e9375bfa701b
Igor Motov <igor@motovs.org>
2016-08-20 19:29:40 -0400

    Fix issues on Linux

--- .oh-my-zsh/custom/linux.zsh
+++ .oh-my-zsh/custom/linux.zsh
@@ -2,4 +2,6 @@ OS=$(uname)
 
 if [[ $OS == "Linux" ]]; then
   M2_HOME=~/Software/apache-maven-3.2.5; export M2_HOME
+  GRADLE_HOME=~/Software/gradle-2.13; export GRADLE_HOME
+  PATH=~/bin:$GRADLE_HOME/bin:$PATH; export PATH
 fi
