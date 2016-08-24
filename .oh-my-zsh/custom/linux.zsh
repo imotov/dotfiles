5a13ee86c73f3f15273c6057fb9f3556681f6a50
Igor Motov <igor@motovs.org>
2016-08-24 19:39:54 -0400

    Add support for nvm on the linux side

--- .oh-my-zsh/custom/linux.zsh
+++ .oh-my-zsh/custom/linux.zsh
@@ -3,5 +3,7 @@ OS=$(uname)
 if [[ $OS == "Linux" ]]; then
   M2_HOME=~/Software/apache-maven-3.2.5; export M2_HOME
   GRADLE_HOME=~/Software/gradle-2.13; export GRADLE_HOME
+  NVM_DIR=~/.nvm; export NVM_DIR
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   PATH=~/bin:$GRADLE_HOME/bin:$PATH; export PATH
 fi
