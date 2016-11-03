4baed89f306e7e775c3f9a297afad587766a2dfb
Igor Motov <igor@motovs.org>
2016-11-03 13:27:47 -1000

    Move gradle from brew to ~/Software

--- .oh-my-zsh/custom/osx.zsh
+++ .oh-my-zsh/custom/osx.zsh
@@ -2,9 +2,9 @@ OS=$(uname)
 
 if [[ $OS == "Darwin" ]]; then
     ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
-    GROOVY_HOME=/usr/local/groovy; export GROOVY_HOME
+    GRADLE_HOME=~/Software/gradle-2.13; export GRADLE_HOME
     JAVA_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_HOME
-    PATH=/usr/local/bin:~/bin:$GROOVY_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
+    PATH=/usr/local/bin:~/bin:$GRADLE_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
     GOPATH=~/Projects/GoWork; export GOPATH
     NVM_DIR=~/.nvm; export NVM_DIR
     source $(brew --prefix nvm)/nvm.sh
