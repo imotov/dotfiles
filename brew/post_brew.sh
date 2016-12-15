29666f048c2162ca676b24041983efd986f79714
Igor Motov <igor@motovs.org>
2016-12-15 10:34:12 -0500

    Remove squid config

--- brew/post_brew.sh
+++ brew/post_brew.sh
@@ -1,8 +1,3 @@
 brew doctor
 brew update
 brew install ant maven32 ivy nvm python3 awscli youtube-dl
-# Setup squid
-brew install squid
-mkdir /usr/local/etc/squid
-~/bin/update_ads.sh
-cp ~/.dotfiles/brew/squid.conf /usr/local/etc
--- brew/push_brew.sh
+++
@@ -1,2 +0,0 @@
-cp ~/.dotfiles/brew/squid.conf /usr/local/etc
-~/bin/update_ads.sh
