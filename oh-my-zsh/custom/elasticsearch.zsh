61f89a77bb14d1f11b98e77db821d5e839748b1b
Igor Motov <igor@motovs.org>
2017-01-11 11:13:05 -0500

    change remote name from elasticsearch to elastic

--- oh-my-zsh/custom/elasticsearch.zsh
+++ oh-my-zsh/custom/elasticsearch.zsh
@@ -1,7 +1,7 @@
 # Elasticsearch specific settings
-alias gffes='git merge --ff-only elasticsearch/$(current_branch)'
+alias gffes='git merge --ff-only elastic/$(current_branch)'
 alias gffor='git merge --ff-only origin/$(current_branch)'
-alias gfees='git fetch elasticsearch'
+alias gfees='git fetch elastic'
 alias gfeor='git fetch origin'
 function gfe {
 	git fetch $1
