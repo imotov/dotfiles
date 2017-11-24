07f73269454d122a54170cc4bc7b340a90eb0163
Igor Motov <igor@motovs.org>
2017-11-23 20:59:43 -0500

    Env cleanup

--- oh-my-zsh/custom/osx.zsh
+++ oh-my-zsh/custom/osx.zsh
@@ -5,9 +5,7 @@ if [[ $OS == "Darwin" ]]; then
     GRADLE_HOME=~/Software/gradle-4.1; export GRADLE_HOME
     JAVA_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_HOME
     PATH=/usr/local/bin:~/bin:$GRADLE_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
-    GOPATH=~/Projects/GoWork; export GOPATH
     NVM_DIR=~/.nvm; export NVM_DIR
     source $(brew --prefix nvm)/nvm.sh
     alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
-    alias agat='for i in img*.tif; do convert -crop 50%x100% +repage $i ${i%.*}%d.jpg; done'
 fi
