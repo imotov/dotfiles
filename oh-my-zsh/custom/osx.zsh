35d28006ddc868fcf59291b4b76486423b650010
Igor Motov <igor@motovs.org>
2017-12-12 21:33:57 -0500

    Go back

--- oh-my-zsh/custom/osx.zsh
+++ oh-my-zsh/custom/osx.zsh
@@ -4,6 +4,7 @@ if [[ $OS == "Darwin" ]]; then
     ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
     GRADLE_HOME=~/Software/gradle-4.1; export GRADLE_HOME
     JAVA_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_HOME
+    GOPATH=~/Projects/GoWork; export GOPATH
     PATH=/usr/local/bin:~/bin:$GRADLE_HOME/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
     NVM_DIR=~/.nvm; export NVM_DIR
     source $(brew --prefix nvm)/nvm.sh
