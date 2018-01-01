ad61ac16be21ccaa14378b56b2286215a64145b3
Igor Motov <igor@motovs.org>
2018-01-01 17:03:08 -0500

    Clean up binaries

--- bin/update_ads.sh
+++
@@ -1,7 +0,0 @@
-#### Calomel.org  ad_servers_newlist.sh 
-#
-## get new ad server list
-curl -sS -L --compressed "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext" > /usr/local/etc/squid/ad_block.txt 
-
-## refresh squid
-/usr/local/sbin/squid -k reconfigure
