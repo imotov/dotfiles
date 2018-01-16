8b81f8abfb200e776bd5a1f76f72a26009c0152c
Igor Motov <igor@motovs.org>
2018-01-16 17:48:17 -0500

    Java 9

--- oh-my-zsh/custom/cicada.zsh
+++ oh-my-zsh/custom/cicada.zsh
@@ -4,7 +4,8 @@ if [[ $NAME == "cicada.local" ]]; then
     ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
     JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_8_HOME
     JAVA_9_HOME=$(/usr/libexec/java_home -v 9.0); export JAVA_9_HOME
-    JAVA_HOME=$JAVA_8_HOME; export JAVA_HOME
+    JAVA_HOME=$JAVA_9_HOME; export JAVA_HOME
+    RUNTIME_JAVA_HOME=$JAVA_8_HOME; export RUNTIME_JAVA_HOME
     GOPATH=~/Projects/GoWork; export GOPATH
     PATH=/usr/local/bin:~/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
     NVM_DIR=~/.nvm; export NVM_DIR
