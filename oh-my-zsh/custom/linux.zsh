cbad32e3fa6409c2c81b3b1d2faa33058ce6bdab
Igor Motov <igor@motovs.org>
2018-01-17 14:19:45 -0500

    Remove gradle and maven paths

--- oh-my-zsh/custom/linux.zsh
+++ oh-my-zsh/custom/linux.zsh
@@ -1,9 +1,6 @@
 OS=$(uname)
 
 if [[ $OS == "Linux" ]]; then
-  M2_HOME=~/Software/apache-maven-3.2.5; export M2_HOME
-  GRADLE_HOME=~/Software/gradle-4.3.1; export GRADLE_HOME
   NVM_DIR=~/.nvm; export NVM_DIR
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-  PATH=~/bin:$GRADLE_HOME/bin:$PATH; export PATH
 fi
