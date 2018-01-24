b2dbe2ec26317690623b41686d9ba7c4aa17ea93
Igor Motov <igor@motovs.org>
2018-01-24 10:30:55 -0500

    Update java settings on gejigeji

--- oh-my-zsh/custom/gejigeji.zsh
+++ oh-my-zsh/custom/gejigeji.zsh
@@ -2,7 +2,9 @@ NAME=$(uname -n)
 
 if [[ $NAME == "gejigeji.local" ]]; then
     JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_8_HOME
-    JAVA_HOME=$(/usr/libexec/java_home -v 9.0); export JAVA_HOME
+    JAVA_9_HOME=$(/usr/libexec/java_home -v 9.0); export JAVA_9_HOME
+    JAVA_HOME=$JAVA_9_HOME; export JAVA_HOME
+    RUNTIME_JAVA_HOME=$JAVA_8_HOME; export RUNTIME_JAVA_HOME
     PATH=/usr/local/bin:~/bin:$PATH; export PATH
     NVM_DIR=~/.nvm; export NVM_DIR
     source $(brew --prefix nvm)/nvm.sh
