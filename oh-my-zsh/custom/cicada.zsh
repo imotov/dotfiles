aa404f8acda7cd4da31b1344b74425efd89a89dd
Igor Motov <igor@motovs.org>
2018-09-12 09:50:09 +0400

    Switch to JAVA<version>_HOME format

--- oh-my-zsh/custom/cicada.zsh
+++ oh-my-zsh/custom/cicada.zsh
@@ -2,17 +2,17 @@ OS=$(uname)
 
 if [[ $OS == "Darwin" ]]; then
     ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
-    JAVA_7_HOME=$(/usr/libexec/java_home -v 1.7); export JAVA_7_HOME
+    JAVA7_HOME=$(/usr/libexec/java_home -v 1.7); export JAVA7_HOME
     launchctl setenv JAVA_7_HOME "$JAVA_7_HOME"
-    JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA_8_HOME
+    JAVA8_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA8_HOME
     launchctl setenv JAVA_8_HOME "$JAVA_8_HOME"
-    JAVA_9_HOME=$(/usr/libexec/java_home -v 9.0); export JAVA_9_HOME
+    JAVA9_HOME=$(/usr/libexec/java_home -v 9.0); export JAVA9_HOME
     launchctl setenv JAVA_9_HOME "$JAVA_9_HOME"
-    JAVA_10_HOME=$(/usr/libexec/java_home -v 10.0); export JAVA_10_HOME
-    launchctl setenv JAVA_10_HOME "$JAVA_10_HOME"
-    JAVA_HOME=$JAVA_10_HOME; export JAVA_HOME
+    JAVA10_HOME=$(/usr/libexec/java_home -v 10.0); export JAVA10_HOME
+    launchctl setenv JAVA_10_HOME "$JAVA10_HOME"
+    JAVA_HOME=$JAVA10_HOME; export JAVA_HOME
     launchctl setenv JAVA_HOME "$JAVA_HOME"
-    RUNTIME_JAVA_HOME=$JAVA_8_HOME; export RUNTIME_JAVA_HOME
+    RUNTIME_JAVA_HOME=$JAVA8_HOME; export RUNTIME_JAVA_HOME
     GOPATH=~/Projects/GoWork; export GOPATH
     PATH=/usr/local/bin:~/bin:$ANDROID_HOME/platform-tools:$PATH; export PATH
     NVM_DIR=~/.nvm; export NVM_DIR
