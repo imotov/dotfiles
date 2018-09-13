acfae1fd52173edff09ab5560af9b0e47b5140ab
Igor Motov <igor@motovs.org>
2018-09-13 14:07:00 +0400

    Fix java env paths on cicada

--- oh-my-zsh/custom/cicada.zsh
+++ oh-my-zsh/custom/cicada.zsh
@@ -3,13 +3,15 @@ OS=$(uname)
 if [[ $OS == "Darwin" ]]; then
     ANDROID_HOME=~/Library/Android/sdk; export ANDROID_HOME
     JAVA7_HOME=$(/usr/libexec/java_home -v 1.7); export JAVA7_HOME
-    launchctl setenv JAVA_7_HOME "$JAVA_7_HOME"
+    launchctl setenv JAVA7_HOME "$JAVA7_HOME"
     JAVA8_HOME=$(/usr/libexec/java_home -v 1.8); export JAVA8_HOME
-    launchctl setenv JAVA_8_HOME "$JAVA_8_HOME"
+    launchctl setenv JAVA8_HOME "$JAVA8_HOME"
     JAVA9_HOME=$(/usr/libexec/java_home -v 9.0); export JAVA9_HOME
-    launchctl setenv JAVA_9_HOME "$JAVA_9_HOME"
+    launchctl setenv JAVA9_HOME "$JAVA9_HOME"
     JAVA10_HOME=$(/usr/libexec/java_home -v 10.0); export JAVA10_HOME
-    launchctl setenv JAVA_10_HOME "$JAVA10_HOME"
+    launchctl setenv JAVA10_HOME "$JAVA10_HOME"
+    JAVA11_HOME=$(/usr/libexec/java_home -v 11.0); export JAVA11_HOME
+    launchctl setenv JAVA11_HOME "$JAVA11_HOME"
     JAVA_HOME=$JAVA10_HOME; export JAVA_HOME
     launchctl setenv JAVA_HOME "$JAVA_HOME"
     RUNTIME_JAVA_HOME=$JAVA8_HOME; export RUNTIME_JAVA_HOME
