5f8eb9b46a89df11b7840d65f0e0eca8675670ec
Igor Motov <igor@motovs.org>
2019-04-03 16:02:34 -0400

    Cleanup bumblebee setup

--- oh-my-zsh/custom/bumblebee.zsh
+++ oh-my-zsh/custom/bumblebee.zsh
@@ -1,8 +1,6 @@
 NAME=$(uname -n)
 
 if [[ $NAME == "bumblebee" ]]; then
-    # Fix zoom on high-res display
-    QT_SCALE_FACTOR=2; export QT_SCALE_FACTOR
     alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
     alias tues="ssh -C -N -T -v -L 5601:127.0.0.1:5601 home"
     alias tuci="ssh -C -N -T -v -L 8080:127.0.0.1:8080 home"
