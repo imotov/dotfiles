a450ab11980fa619e37e336a387e9ae735d9df86
Igor Motov <igor@motovs.org>
2019-04-04 13:55:55 -0400

    Cleanup ladybug as well

--- oh-my-zsh/custom/ladybug.zsh
+++ oh-my-zsh/custom/ladybug.zsh
@@ -1,8 +1,6 @@
 NAME=$(uname -n)
 
 if [[ $NAME == "ladybug" ]]; then
-    # Fix zoom on high-res display
-    QT_SCALE_FACTOR=2; export QT_SCALE_FACTOR
     # Setup go paths
     GOPATH=~/Projects/GoWork; export GOPATH
     PATH=$PATH:$GOPATH/bin; export PATH
