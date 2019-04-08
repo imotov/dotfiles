c9ae956c57305fd57e305b5e83f5e67b077167bc
Igor Motov <igor@motovs.org>
2019-04-08 17:10:46 +0400

    Remove bumblebee-specific script

--- oh-my-zsh/custom/bumblebee.zsh
+++
@@ -1,7 +0,0 @@
-NAME=$(uname -n)
-
-if [[ $NAME == "bumblebee" ]]; then
-    alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
-    alias tues="ssh -C -N -T -v -L 5601:127.0.0.1:5601 home"
-    alias tuci="ssh -C -N -T -v -L 8080:127.0.0.1:8080 home"
-fi
