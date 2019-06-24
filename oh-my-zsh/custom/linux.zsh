40ac665ab96da6d2bc04b076b283f6f4e72e5566
Igor Motov <igor@motovs.org>
2019-06-24 09:06:04 -0400

    Add aliases to adjust video mode on USB-C display

--- oh-my-zsh/custom/linux.zsh
+++ oh-my-zsh/custom/linux.zsh
@@ -6,4 +6,7 @@ if [[ $OS == "Linux" ]]; then
   alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
   alias tues="ssh -C -N -T -v -L 5601:127.0.0.1:5601 home"
   alias tuci="ssh -C -N -T -v -L 8080:127.0.0.1:8080 home"
+  # adjust USB-C display
+  alias ucdon='xrandr --output DP-1 --scale 2x2'
+  alias ucdoff='xrandr --output DP-1 --scale 1x1'
 fi
