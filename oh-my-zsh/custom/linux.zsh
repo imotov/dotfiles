a454e709c2a61395ac8f9feac647dbe20b855728
Igor Motov <igor@motovs.org>
2019-06-25 10:41:03 -0400

    Add gopath on all linux machines

--- oh-my-zsh/custom/linux.zsh
+++ oh-my-zsh/custom/linux.zsh
@@ -3,6 +3,8 @@ OS=$(uname)
 if [[ $OS == "Linux" ]]; then
   NVM_DIR=~/.nvm; export NVM_DIR
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  GOPATH=~/Projects/GoWork; export GOPATH
+  PATH=$PATH:$GOPATH/bin; export PATH  
   alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
   alias tues="ssh -C -N -T -v -L 5601:127.0.0.1:5601 home"
   alias tuci="ssh -C -N -T -v -L 8080:127.0.0.1:8080 home"
