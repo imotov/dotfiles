467cd0de909161d32bab0b724200dde331d7dc70
Igor Motov <igor@motovs.org>
2019-07-28 14:11:34 -0400

    Remove elastic stack setup packages

--- setup/elastic_stack_remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Don't continue if anything goes wrong
-set -e
-
-sudo apt-get remove elasticsearch kibana
-sudo rm -rf /var/lib/elasticsearch
-sudo rm -rf /etc/elasticsearch
-sudo rm -rf /var/lib/kibana
-sudo rm -rf /etc/kibana
