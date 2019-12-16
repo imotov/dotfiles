adaddf2f641fdb93f2cf71d42273e3cf1cc0d926
Igor Motov <igor@motovs.org>
2019-12-16 17:29:47 -0500

    Add XPS's battery saving scripts

--- oh-my-zsh/custom/linux.zsh
+++ oh-my-zsh/custom/linux.zsh
@@ -11,4 +11,9 @@ if [[ $OS == "Linux" ]]; then
   # adjust USB-C display
   alias ucdon='xrandr --output DP-1 --scale 2x2'
   alias ucdoff='xrandr --output DP-1 --scale 1x1'
+batsave() {
+  sudo sh -c 'echo "min_power" > "/sys/class/scsi_host/host1/link_power_management_policy"'
+  sudo sh -c 'echo "min_power" > "/sys/class/scsi_host/host0/link_power_management_policy"'
+  sudo sh -c 'echo "1500" > "/proc/sys/vm/dirty_writeback_centisecs"'
+}
 fi
