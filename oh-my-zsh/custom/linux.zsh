d9cd422f4448bf0fd3d2ac9aae4a78cbab7d81d7
Igor Motov <igor@motovs.org>
2020-04-11 11:44:35 -0400

    Add branch cleanup script

--- oh-my-zsh/custom/linux.zsh
+++ oh-my-zsh/custom/linux.zsh
@@ -16,4 +16,27 @@ batsave() {
   sudo sh -c 'echo "min_power" > "/sys/class/scsi_host/host0/link_power_management_policy"'
   sudo sh -c 'echo "1500" > "/proc/sys/vm/dirty_writeback_centisecs"'
 }
+clean_branches() {
+  branches=()
+  eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+  for branch in "${branches[@]}"; do
+      short_name=${branch#"refs/heads/"}
+      echo ">>>> " $short_name
+      git --no-pager log "$branch" -1
+      read -q "yn?Delete this branch? "
+      echo
+      case $yn in
+      [Nn]* )
+          echo
+          echo keeping
+          echo
+          ;;
+      [Yy]* )
+          echo
+          git branch -D $short_name
+          echo
+          ;;
+      esac
+  done
+}
 fi
