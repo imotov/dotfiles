37faa5aa073dca52a13cd03c24795b659740e642
Igor Motov <igor@motovs.org>
2020-05-05 09:07:37 -0400

    Add gprm and gprc commands for code reviews

--- oh-my-zsh/custom/elasticsearch.zsh
+++ oh-my-zsh/custom/elasticsearch.zsh
@@ -12,8 +12,17 @@ function gff {
 function gpr {
     git fetch elastic pull/$1/head:pr/$1
 }
+# fetch PR and squash merge it into master
+function gprm {
+	gpr $1 && git merge --squash pr/$1
+}
+# clean after PR review
+function gprc {
+	git branch -D pr/$1 && git reset --hard HEAD
+}
+
 alias gnow='git commit --amend --date "`date`"'
 # usage rempush: remote
 # leafminer tests
-alias leafpush='git push -f leafminer $(current_branch)'
-alias homepush='git push -f home $(current_branch); ssh home "cd /media/ramdisk; if ! test -e ${PWD##~/Projects/*/}; then git clone ~/Repo/${PWD##*/} ${PWD##~/Projects/*/}; fi; cd ${PWD##~/Projects/*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch)"'
+# alias leafpush='git push -f leafminer $(current_branch)'
+# alias homepush='git push -f home $(current_branch); ssh home "cd /media/ramdisk; if ! test -e ${PWD##~/Projects/*/}; then git clone ~/Repo/${PWD##*/} ${PWD##~/Projects/*/}; fi; cd ${PWD##~/Projects/*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch)"'
