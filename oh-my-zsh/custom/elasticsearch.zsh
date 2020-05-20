2a18ec351f2fcb67f8b44f10b501dd761b7b601d
Igor Motov <igor@motovs.org>
2020-05-20 19:01:14 -0400

    Add rampush and cicheck functions

--- oh-my-zsh/custom/elasticsearch.zsh
+++ oh-my-zsh/custom/elasticsearch.zsh
@@ -26,3 +26,20 @@ alias gnow='git commit --amend --date "`date`"'
 # leafminer tests
 # alias leafpush='git push -f leafminer $(current_branch)'
 # alias homepush='git push -f home $(current_branch); ssh home "cd /media/ramdisk; if ! test -e ${PWD##~/Projects/*/}; then git clone ~/Repo/${PWD##*/} ${PWD##~/Projects/*/}; fi; cd ${PWD##~/Projects/*/}; git fetch origin; git checkout $(current_branch); git reset --hard origin/$(current_branch)"'
+function rampush {
+	cur_dir=${PWD}
+	cur_proj=${PWD##*/}
+	cur_branch=$(current_branch)
+	cd /mnt/ramdisk
+	if ! test -e ${cur_proj}; then
+		git clone ${cur_dir}
+	fi
+	cd ${cur_proj}
+	git fetch origin
+	git reset --hard origin/${cur_branch}
+	cd ${cur_dir}
+}
+# run common PR checks
+function prcheck {
+	./gradlew --parallel --max-workers=14 checkPart1 checkPart2 bwcTestSnapshots
+}
