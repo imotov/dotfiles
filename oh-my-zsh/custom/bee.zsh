48f828dc77697051527f2928e6cee13c7df1f416
Igor Motov <igor@motovs.org>
2021-06-01 07:26:07 -1000

    Add java env variables on bee

--- oh-my-zsh/custom/bee.zsh
+++ oh-my-zsh/custom/bee.zsh
@@ -4,4 +4,13 @@ if [[ $OS == "Darwin" ]]; then
   alias callhome="ssh -C -N -2 -v -D 8080 home -p 22422"
   alias tues="ssh -C -N -T -v -L 5601:127.0.0.1:5601 home"
   alias tuci="ssh -C -N -T -v -L 8080:127.0.0.1:8080 home"
+
+  JAVA11_HOME=$(/usr/libexec/java_home -v 11.0); export JAVA11_HOME
+  launchctl setenv JAVA11_HOME "$JAVA11_HOME"
+
+  JAVA16_HOME=$(/usr/libexec/java_home -v 16.0); export JAVA16_HOME
+  launchctl setenv JAVA16_HOME "$JAVA16_HOME"
+
+  JAVA_HOME=$JAVA16_HOME; export JAVA_HOME
+  launchctl setenv JAVA_HOME "$JAVA_HOME"
 fi
