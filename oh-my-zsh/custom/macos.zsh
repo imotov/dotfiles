cea3b8439e63a450cfa588c24677b9b6c28714c5
Igor Motov <igor@motovs.org>
2021-12-16 17:27:17 -1000

    Cleanup macos files

--- oh-my-zsh/custom/macos.zsh
+++ oh-my-zsh/custom/macos.zsh
@@ -16,6 +16,9 @@ if [[ $OS == "Darwin" ]]; then
 
   JAVA_HOME=$JAVA17_HOME; export JAVA_HOME
   launchctl setenv JAVA_HOME "$JAVA_HOME"
-  
   eval "$(/opt/homebrew/bin/brew shellenv)"
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
