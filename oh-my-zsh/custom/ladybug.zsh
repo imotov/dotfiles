38564d49c87f1091f2b75dfb89bff6240801caf2
Igor Motov <igor@motovs.org>
2021-12-19 18:07:39 -1000

    Cleanup zsh custom files

--- oh-my-zsh/custom/ladybug.zsh
+++
@@ -1,8 +0,0 @@
-NAME=$(uname -n)
-
-if [[ $NAME == "ladybug" ]]; then
-  NVM_DIR=~/.nvm; export NVM_DIR
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-  GOPATH=~/Projects/GoWork; export GOPATH
-  PATH=$PATH:$GOPATH/bin:$HOME/.cargo/bin; export PATH
-fi
