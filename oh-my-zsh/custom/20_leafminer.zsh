a4fbc322148e72a4de56f8b3f6e36fb5da5929f9
Igor Motov <igor@motovs.org>
2021-12-23 16:37:48 -1000

    Update leafminer script

---
+++ oh-my-zsh/custom/20_leafminer.zsh
@@ -0,0 +1,9 @@
+OS=$(uname)
+
+if [[ $NAME == "leafminer" ]]; then
+  PYENV_ROOT="$HOME/.pyenv"
+  PATH="$PYENV_ROOT/bin:$PATH"
+  if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init --path)"
+  fi
+fi
