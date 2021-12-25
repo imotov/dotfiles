ee339ffee52a8cd5698e51d29dd589284e9dab7b
Igor Motov <igor@motovs.org>
2021-12-25 09:00:06 -1000

    Fix docker install

--- setup/leafminer.sh
+++ setup/leafminer.sh
@@ -25,20 +25,14 @@ sudo apt-get install -y\
 curl -sk https://raw.githubusercontent.com/imotov/dotfiles/master/bootstrap | zsh
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo gpasswd -a $USER docker
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-# setup dotfiles and enviroments
-git clone -q git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cd ~/.oh-my-zsh/plugins && git clone -q git://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone -q https://github.com/imotov/dotfiles.git ~/.dotfiles
-. ~/.dotfiles/push
 
 pushd ~/.dotfiles
 # Set proper url for .dotfiles remote repository
