13f3309feae185e7b95001b6deb88bad02b96241
Igor Motov <igor@motovs.org>
2022-11-11 14:29:27 -1000

    Make java setup conditional

--- oh-my-zsh/custom/00_macos.zsh
+++ oh-my-zsh/custom/00_macos.zsh
@@ -7,17 +7,20 @@ if [[ $OS == "Darwin" ]]; then
   alias tuci="ssh -C -N -T -v -L 8080:127.0.0.1:8080 home"
 
   # Setup java
-  JAVA11_HOME=$(/usr/libexec/java_home -v 11.0); export JAVA11_HOME
-  launchctl setenv JAVA11_HOME "$JAVA11_HOME"
+  /usr/libexec/java_home &> /dev/null
+  if [ $? -eq 0 ]; then
+    JAVA11_HOME=$(/usr/libexec/java_home -v 11.0); export JAVA11_HOME
+    launchctl setenv JAVA11_HOME "$JAVA11_HOME"
 
-  JAVA16_HOME=$(/usr/libexec/java_home -v 16.0); export JAVA16_HOME
-  launchctl setenv JAVA16_HOME "$JAVA16_HOME"
+    JAVA16_HOME=$(/usr/libexec/java_home -v 16.0); export JAVA16_HOME
+    launchctl setenv JAVA16_HOME "$JAVA16_HOME"
 
-  JAVA17_HOME=$(/usr/libexec/java_home -v 17.0); export JAVA16_HOME
-  launchctl setenv JAVA17_HOME "$JAVA17_HOME"
+    JAVA17_HOME=$(/usr/libexec/java_home -v 17.0); export JAVA16_HOME
+    launchctl setenv JAVA17_HOME "$JAVA17_HOME"
 
-  JAVA_HOME=$JAVA17_HOME; export JAVA_HOME
-  launchctl setenv JAVA_HOME "$JAVA_HOME"
+    JAVA_HOME=$JAVA17_HOME; export JAVA_HOME
+    launchctl setenv JAVA_HOME "$JAVA_HOME"
+  fi
 
   # Setup homebrew
   [ -s "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
