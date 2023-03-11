3fec60879e1f8c3f3c3cc7922e2e24a4f4ba8761
Igor Motov <igor@motovs.org>
2023-03-11 12:53:52 -1000

    Use origin in standard way

--- oh-my-zsh/custom/10_dev.zsh
+++ oh-my-zsh/custom/10_dev.zsh
@@ -1,10 +1,10 @@
-# Github specific settings
+# ========= Github specific settings ========= 
 alias gffor='git merge --ff-only origin/$(current_branch)'
 alias gfeor='git fetch origin'
-alias gffqw='git merge --ff-only quickwit-oss/$(current_branch)'
-alias gfeqw='git fetch quickwit-oss'
-alias cds='cd ~/Software'
-alias cdd='cd ~/Downloads'
+alias gffim='git merge --ff-only imotov/$(current_branch)'
+alias gfeim='git fetch imotov'
+alias gimpush='git push imotov "$(git_current_branch)"'
+
 function gfe {
 	git fetch $1
 }
@@ -12,18 +12,22 @@ function gff {
 	git merge --ff-only $1/$(current_branch)
 }
 
-alias gnow='git commit --amend --date "`date`"'
-
-function shuffle {
-  for i; do
-    echo $i
-  done \
-  | awk 'BEGIN{srand()}{print rand(), $0}' \
-  | sort -n -k 1 \
-  | awk 'sub(/^[0-9.]+ ([a-zA-Z0-9]+)$/,$2)' \
-  | tr '\n' ' '
-	echo
+# fetch PR from remote 
+function gprf {
+  git fetch origin pull/$1/head:pr/$1
 }
+
+# fetch PR and squash merge it into master
+function gprm {
+  gprf $1 && git merge --squash pr/$1
+}
+
+# clean after PR review
+function gprc {
+  git branch -D pr/$1 && git reset --hard HEAD
+}
+
+alias gnow='git commit --amend --date "`date`"'
 
 # Git create ssh remote 
 function gcsr {
@@ -39,3 +43,24 @@ function gcsr {
   ssh $server "set -e; cd $git_path; mkdir $repository; cd $repository; git init --initial-branch=main --bare"
   git remote add $server ${user}@$server:${git_path}/${repository}
 }
+
+# ========= Misc commands ========= 
+
+function shuffle {
+  for i; do
+    echo $i
+  done \
+  | awk 'BEGIN{srand()}{print rand(), $0}' \
+  | sort -n -k 1 \
+  | awk 'sub(/^[0-9.]+ ([a-zA-Z0-9]+)$/,$2)' \
+  | tr '\n' ' '
+	echo
+}
+
+# Working directories
+alias cds='cd ~/Software'
+alias cdd='cd ~/Downloads'
+alias cdsb='cd ~/Sandbox'
+alias cdp='cd ~/Projects'
+alias cdpi='cd ~/Projects/imotov'
+alias cdpq='cd ~/Projects/quickwit-oss'
