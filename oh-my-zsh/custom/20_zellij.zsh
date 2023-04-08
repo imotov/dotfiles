57edff757f972759509463d029f9637dbe6031ed
Igor Motov <igor@motovs.org>
2023-04-08 11:34:21 -1000

    Fix zellij setup

--- oh-my-zsh/custom/20_zellij.zsh
+++ oh-my-zsh/custom/20_zellij.zsh
@@ -1,7 +1,7 @@
-  # Setup zellij environment
-  if [ -x "$HOME/.cargo/bin/zellij" ]; then
-    alias z="zellij"
-    alias za="zellij a -c default"
-    alias zls="zellij ls"
-    alias zka="zellij ka"
-  fi
+# Setup zellij environment
+if [[ -x "$HOME/.cargo/bin/zellij" ||  -x "/usr/local/bin/zellij" ]]; then
+  alias z="zellij"
+  alias za="zellij a -c default"
+  alias zls="zellij ls"
+  alias zka="zellij ka"
+fi
