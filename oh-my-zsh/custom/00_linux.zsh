655760ea072a5d56e1ee8ef99c17875bc33cd4b0
Igor Motov <igor@motovs.org>
2023-05-18 10:16:15 -1000

    Setup nvm on linux

--- oh-my-zsh/custom/00_linux.zsh
+++ oh-my-zsh/custom/00_linux.zsh
@@ -9,4 +9,12 @@ if [[ $OS == "Linux" ]]; then
     eval "$(pyenv virtualenv-init -)"
     export PYENV_VIRTUALENV_DISABLE_PROMPT=1
   fi
+
+  # Setup node.js virtual environment manager
+  if [ -s "$HOME/.nvm/nvm.sh" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  fi
+
 fi
